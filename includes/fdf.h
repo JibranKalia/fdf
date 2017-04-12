396836027d174a39242ddcb289f6e3a79a019e7b
Jibran Kalia <jibran.kalia@gmail.com>
2017-04-12 11:49:18 -0700

    Simple work

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,11 +6,18 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/29 17:36:31 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/29 17:37:01 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/12 11:46:40 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
+typedef struct	s_env
+{
+	void		*mlx;
+	void		*win;
+	void		*img;
+}				t_env;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,20 +6,19 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/04 18:07:58 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/04 18:11:09 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/12 11:47:44 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fdf.h>
 #include <mlx.h>
 
-int	main(void)
+int		main(void)
 {
-	void	*mlx;
-	void	*window;
+	t_env	env;
 
-	mlx = mlx_init();
-	window = mlx_new_window(mlx, 1000, 1000, "Title");
-
-	mlx_loop(mlx);
+	env.mlx = mlx_init();
+	env.win = mlx_new_window(env.mlx, 1000, 1000, "Title");
+	mlx_loop(env.mlx);
 	return (0);
 }

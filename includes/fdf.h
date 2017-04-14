d123d713c116ae9cfcec0d30e6186ab9a08c80c6
Jibran Kalia <jibran.kalia@gmail.com>
2017-04-13 18:34:30 -0700

    Minor changes

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,12 +6,18 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/29 17:36:31 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/12 11:46:40 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/12 12:20:19 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+# define W_WIDTH 1600
+# define W_HEIGHT 900
+
+# include <stdlib.h>
+# include <mlx.h>
+# include <libft.h>
 
 typedef struct	s_env
 {
--- src/main.c
+++ src/main.c
@@ -6,19 +6,41 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/04 18:07:58 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/12 11:47:44 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/12 12:19:17 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
 #include <mlx.h>
 
+#define ESC			53
+#define W			13
+#define S			1
+#define A			0
+#define D			2
+#define UP			126
+#define DOWN		125
+#define RIGHT		124
+#define LEFT		123
+#define PAGE_UP		116
+#define PAGE_DOWN	121
+
+int		key_hooks(int keycode, t_env *env)
+{
+	if (keycode == ESC)
+		exit(0);
+	(void)env;
+	return (0);
+}
+
 int		main(void)
 {
 	t_env	env;
 
 	env.mlx = mlx_init();
-	env.win = mlx_new_window(env.mlx, 1000, 1000, "Title");
+	env.win = mlx_new_window(env.mlx, W_WIDTH, W_HEIGHT, "FDF");
+	env.img = mlx_new_image(env.mlx, W_WIDTH, W_HEIGHT);
+	mlx_key_hook(env.win, key_hooks, &env);
 	mlx_loop(env.mlx);
 	return (0);
 }

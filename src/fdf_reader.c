2bcc8f96ae5968657518661c6b75d3a9819c0393
Jibran Kalia <jkalia@student.42.us.org>
2017-05-09 14:59:01 -0700

    Switched to dynamic array

--- src/fdf_reader.c
+++ src/fdf_reader.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/14 10:20:09 by jkalia            #+#    #+#             */
-/*   Updated: 2017/05/09 14:02:37 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/05/09 14:56:05 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,29 +24,57 @@ void		map_clean(void *elm)
 	ft_bzero(elm, sizeof(t_point));
 }
 
-static int	read_point(char *src, t_env *env)
+
+//void		file_clean(void *elm)
+//{
+//	free(elm);
+//}
+
+//static int	check_point(t_env *env)
+//{
+//	t_point	**tmp;
+//	int		i;
+//
+//	i = 0;
+//	tmp = (t_point **)env->map->contents;
+//	while (i < env->map->end)
+//	{
+//		printf("x = %.0f\n", tmp[i]->x);
+//		printf("y = %.0f\n", tmp[i]->y);
+//		printf("z = %.0f\n", tmp[i]->z);
+//		++i;
+//	}
+//	return (0);
+//}
+
+static int	read_point(t_arr *src, t_env *env)
 {
 	char		**tmp;
+	char		**out;
 	t_point		*in;
-	int	i;
-	int	x;
-	int	y;
+	int			i;
+	int			j;
 
 	env->map = arr_create(sizeof(t_point), env->h * env->w);
 	MEMCHECK(env->map);
 	env->map->del = map_clean;
-	tmp = ft_strsplit(src, ' ');
+	tmp = (char **)src->contents;
 	i = 0;
 	while (tmp[i])
 	{
-		in = ft_memalloc(sizeof(t_point));
-		MEMCHECK(in);
-		y = i / env->w;
-		x = i - (env->w * y);
-		in->x = (double)x + 1;
-		in->y = (double)y + 1;
-		in->z = (double)ft_atoi(tmp[i]);
-		arr_push(env->map, in);
+		out = ft_strsplit(tmp[i], ' ');
+		j = 0;
+		while (out[j])
+		{	
+			in = ft_memalloc(sizeof(t_point));
+			MEMCHECK(in);
+			in->x = (double)j + 1;
+			in->y = (double)i + 1;
+ 			in->z = ft_atoi(out[j]);
+			arr_push(env->map, in);
+			++j;
+		}
+		ft_tbldel(out);
 		++i;
 	}
 	return (0);
@@ -55,29 +83,31 @@ static int	read_point(char *src, t_env *env)
 int			fdf_reader(t_env *env, int fd)
 {
 	char	*line;
-	char	*ret;
 	int		count;
 	int		b;
+	t_arr	*file;
 
+	file = arr_create(sizeof(char*), 100);
+	MEMCHECK(file);
+//	file->del = file_clean;
 	count = 0;
 	while ((b = get_next_line(fd, &line)))
 	{
 		CHECK(b == -1, RETURN(-1), "Read Failed");
 		if (count == 0)
 		{
-			ret = ft_strdup(line);
-			env->w = ft_countwords(ret, ' ');
+			arr_push(file, ft_strdup(line));
+			env->w = ft_countwords(line, ' ');
 		}
 		else
-		{
-			ret = ft_strjoin(ret, " ");
-			ret = ft_strjoin(ret, line);
-		}
+			arr_push(file, ft_strdup(line));
 		CHECK(ft_countwords(line, ' ') != env->w, RETURN(-1), "Error in FDF file");
 		++count;
 		free(line);
 	}
 	env->h = count;
-	read_point(ret, env);
+	read_point(file, env);
+//	check_point(env);
+	arr_del(file);
 	return (0);
 }
